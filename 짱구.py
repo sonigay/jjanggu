c99a3cd47e35a62de06c8f93e3cbb5882d8c713e
sonigay <43840048+sonigay@users.noreply.github.com>
2019-12-29 02:45:44 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -123,6 +123,60 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)       
         
         
+        
+         if message.content.startswith("!명령어"):
+        channel = message.channel
+        embed = discord.Embed(
+            title = '명령어들이다 크크크큭',
+            description = '각각의 명령어들 이다 잘 봐둬라 큭...',
+            colour = discord.Colour.blue()
+        )
+
+        #embed.set_footer(text = '끗')
+        dtime = datetime.datetime.now()
+        #print(dtime[0:4]) # 년도
+        #print(dtime[5:7]) #월
+        #print(dtime[8:11])#일
+        #print(dtime[11:13])#시
+        #print(dtime[14:16])#분
+        #print(dtime[17:19])#초
+        embed.set_footer(text=str(dtime.year)+"년 "+str(dtime.month)+"월 "+str(dtime.day)+"일 "+str(dtime.hour)+"시 "+str(dtime.minute)+"분 "+str(dtime.second)+"초")
+        #embed.set_footer(text=dtime[0:4]+"년 "+dtime[5:7]+"월 "+dtime[8:11]+"일 "+dtime[11:13]+"시 "+dtime[14:16]+"분 "+dtime[17:19]+"초")
+        embed.add_field(name = '!안녕', value = '인사하고싶을때 ㄱㄱ',inline = False)
+        embed.add_field(name='!오늘배그', value='오늘 배그각 알려줌', inline=False)
+        embed.add_field(name='!홋치', value='!홋치 단어1 단어2 형식으로 적으면 학습함', inline=False)
+        embed.add_field(name='!말해', value='!말해 단어1 형식으로 적으면 학습한내용 말함', inline=False)
+        embed.add_field(name='!기억초기화', value='학습한 데이터 초기화함', inline=False)
+        embed.add_field(name='!데이터목록', value='학습한 데이터목록 알려줌', inline=False)
+        embed.add_field(name='!모두모여', value='모두를 언급함', inline=False)
+        embed.add_field(name='!들어와', value='봇이 음성채널에 들어옴', inline=False)
+        embed.add_field(name='!나가', value='봇이 음성채널에 나감', inline=False)
+        embed.add_field(name='!재생', value='!재생 유튜브링크 형식으로 적으면 유튜브 틀어줌', inline=False)
+        embed.add_field(name='!일시정지', value='재생중인 유튜브 일시정지함', inline=False)
+        embed.add_field(name='!다시재생', value='정지중인 유튜브 다시 재생함', inline=False)
+        embed.add_field(name='!멈춰', value='재생,정지중인 유튜브 없어짐(영상목록 초기화)', inline=False)
+        embed.add_field(name='!날씨', value='!날씨 원하는지역 을 입력하면 그 지역의 날씨정보를 제공합니다.', inline=False)
+        embed.add_field(name='!롤', value='!롤 닉네임 형식으로 적으면 그 닉네임에대한 정보를 알려줍니다..', inline=False)
+        embed.add_field(name='!배그솔로', value='!배그솔로 닉네임 형식으로 적으면 그 닉네임에대한 정보를 알려줍니다..', inline=False)
+        embed.add_field(name='!배그듀오', value='!배그듀오 닉네임 형식으로 적으면 그 닉네임에대한 정보를 알려줍니다..', inline=False)
+        embed.add_field(name='!배그스쿼드', value='!배그스쿼드 닉네임 형식으로 적으면 그 닉네임에대한 정보를 알려줍니다..', inline=False)
+        embed.add_field(name='!고양이', value='!고양이 라고 적으면 고양이 사진이 나옵니다..', inline=False)
+        embed.add_field(name='!강아지', value='!강아지 라고 적으면 강아지 사진이 나옵니다.', inline=False)
+        embed.add_field(name='!네코', value='!네코 라고 적으면 2D 고양이 이미지가 나옵니다', inline=False)
+        embed.add_field(name='!실시간검색어, !실검', value='!실시간검색어, !실검 이라고 적으면 네이버의 실시간 검색어 순위가 나타납니다.', inline=False)
+        embed.add_field(name='!번역 번역할문자', value='!번역 번역할문자 이라고 적으면 번역할 문자를 번역한 링크가 나타납니다. ("띄어쓰기를 하시면 안됩니다. _,-등으로 구분해주세요.")', inline=False)
+        embed.add_field(name='!영화순위', value='영화를 1~20순위로 나눈 영화순위 정보를 제공합니다.', inline=False)
+        embed.add_field(name='!급식', value='군포e비즈니스 고등학교의 급식정보를 제공합니다.', inline=False)
+        embed.add_field(name='!복권', value='랜덤으로 선정한 복권번호를 메시지로 보내줍니다.', inline=False)
+        embed.add_field(name='!검색', value='!검색 검색할키워드 형식으로 입력하시면 유튜브 검색결과를 메시지로 보내줍니다.', inline=False)
+
+        await client.send_message(channel,embed=embed)
+
+    if message.content.startswith("!모두모여"):
+        await client.send_message(message.channel, "@everyone")
+        
+        
+        
          if message.content.startswith("!들어와"):
         channel = message.author.voice.voice_channel
         server = message.server
