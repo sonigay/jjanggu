b6e602493b03ecbfbe224efe766c10c019c04511
sonigay <43840048+sonigay@users.noreply.github.com>
2019-12-28 15:48:01 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -77,6 +77,46 @@ async def on_message(message):
         for i in range(0, len(person)):
             await client.send_message(message.channel, person[i] + "---->" + teamname[i])   
             
+            
+     if message.content.startswith("!복권"):
+        Text = ""
+        number = [1, 2, 3, 4, 5, 6, 7] # 배열크기 선언해줌
+        count = 0
+        for i in range(0, 7):
+            num = random.randrange(1, 46)
+            number[i] = num
+            if count >= 1:
+                for i2 in range(0, i):
+                    if number[i] == number[i2]:  # 만약 현재랜덤값이 이전숫자들과 값이 같다면
+                        numberText = number[i]
+                        print("작동 이전값 : " + str(numberText))
+                        number[i] = random.randrange(1, 46)
+                        numberText = number[i]
+                        print("작동 현재값 : " + str(numberText))
+                        if number[i] == number[i2]:  # 만약 다시 생성한 랜덤값이 이전숫자들과 또 같다면
+                            numberText = number[i]
+                            print("작동 이전값 : " + str(numberText))
+                            number[i] = random.randrange(1, 46)
+                            numberText = number[i]
+                            print("작동 현재값 : " + str(numberText))
+                            if number[i] == number[i2]:  # 만약 다시 생성한 랜덤값이 이전숫자들과 또 같다면
+                                numberText = number[i]
+                                print("작동 이전값 : " + str(numberText))
+                                number[i] = random.randrange(1, 46)
+                                numberText = number[i]
+                                print("작동 현재값 : " + str(numberText))
+
+            count = count + 1
+            Text = Text + "  " + str(number[i])
+
+        print(Text.strip())
+        embed = discord.Embed(
+            title="복권 숫자!",
+            description=Text.strip(),
+            colour=discord.Color.red()
+        )
+        await client.send_message(message.channel, embed=embed)       
+            
 
 
 access_token = os.environ["BOT_TOKEN"]
