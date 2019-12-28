0ad8b56ae8ff6a73a0d58543c7d20ce260e5738d
sonigay <43840048+sonigay@users.noreply.github.com>
2019-12-29 02:57:38 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -122,9 +122,38 @@ async def on_message(message):
         )
         await client.send_message(message.channel, embed=embed)       
         
+          ################ 들어와 ################ 
         
-        
-       
+       if message.content.startswith("!들어와"):
+        channel = message.author.voice.voice_channel
+        server = message.server
+        voice_client = client.voice_client_in(server)
+        print("들어와")
+        print(voice_client)
+        print("들어와")
+        if voice_client== None:
+            await client.send_message(message.channel, '들어왔습니다') 
+            await client.join_voice_channel(channel)
+        else:
+            await client.send_message(message.channel, '봇이 이미 들어와있습니다.') 
+            
+            
+            ################ 나가 ################ 
+            
+        if message.content.startswith("!나가"):
+            server = message.server
+            voice_client = client.voice_client_in(server)
+            print("나가")
+            print(voice_client)
+            print("나가")
+            if voice_client == None:
+                await client.send_message(message.channel,'봇이 음성채널에 접속하지 않았습니다.') # 원래나가있었음 바보녀석 니녀석의 죄는 "어리석음" 이라는 .것.이.다.
+                pass
+            else:
+                await client.send_message(message.channel, '나갑니다') # 나가드림
+                await voice_client.disconnect()        
+            
+            
             
             
 
