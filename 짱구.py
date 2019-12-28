c71edb2c4eb2420b482d4595ab2618d10a48023a
sonigay <43840048+sonigay@users.noreply.github.com>
2019-12-28 12:42:11 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -2,62 +2,20 @@ import discord
 import asyncio
 import random
 import os
-
-
-client = discord.Client()
-
-
-
-@client.event
-async def on_ready():
-    print("login")
-    print(client.user.name)
-    print(client.user.id)
-    print("----------------")
-    await client.change_presence(game=discord.Game(name='당첨자뽑기', type=1))
-
-
-
-@client.event
-async def on_message(message):
-
-     if message.content.startswith('!주사위'):
-        roll = message.content.split(" ")
-        rolld = roll[1].split("d")
-        dice = 0
-        for i in range(1, int(rolld[0])+1):
-            dice = dice + random.randint(1, int(rolld[1]))
-        await client.send_message(message.channel, str(dice))
-
-     if message.content.startswith('!한명뽑기'):
-        await client.send_message(message.channel, "이번 당첨자 분은")   
-                      
-     if message.content.startswith('!한명뽑기'):
-        choice = message.content.split(" ")
-        choicenumber = random.randint(1, len(choice)-1)
-        choiceresult = choice[choicenumber]
-        await client.send_message(message.channel, choiceresult)
-        
-     if message.content.startswith('!뭐먹지'):
-        food = "짜장면 짬뽕 라면 밥 굶기"
-        foodchoice = food.split(" ")
-        foodnumber = random.randint(1, len(foodchoice))
-        foodresult = foodchoice[foodnumber-1]
-        await client.send_message(message.channel, foodresult)
-        
-     if message.content.startswith('!여러명뽑기'):
-        await client.send_message(message.channel, "이번 당첨자 분들은")  
-
-     if message.content.startswith("!여러명뽑기"):
-        team = message.content[7:]
-        peopleteam = team.split("/")
-        people = peopleteam[0]
-        team = peopleteam[1]
-        person = people.split(" ")
-        teamname = team.split(" ")
-        random.shuffle(teamname)
-        for i in range(0, len(person)):
-            await client.send_message(message.channel, person[i] + "---->" + teamname[i])   
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+scope = [
+'https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/drive',
+]
+json_file_name = 'vast-ethos-251302-b8a92651b359.json'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
+gc = gspread.authorize(credentials)
+spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1j83Zex9AdiHad-LkmnndHdvZdu25KupZCmMdtqwCjCY/edit#gid=0'
+# 스프레스시트 문서 가져오기 
+doc = gc.open_by_url(spreadsheet_url)
+# 시트 선택하기
+worksheet = doc.worksheet('시트1')
  
 
 
