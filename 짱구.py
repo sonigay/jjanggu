3ad87ce72fa8ddb1dd5e144ad82e3993986f2dad
sonigay <43840048+sonigay@users.noreply.github.com>
2019-12-30 21:31:32 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -18,26 +18,6 @@ import urllib.request
 
 
 
-import gspread
-
-from oauth2client.service_account import ServiceAccountCredentials
-
-
-scope = [
-    'https://spreadsheets.google.com/feeds',
-    'https://www.googleapis.com/auth/drive',
-]
-json_file_name = 'jungsanfile-e5ae2dbc8879.json'
-credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
-gc = gspread.authorize(credentials)
-spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1WzFr_aNi6T_ievrOaVJK3hT2tRQsjltfACC6WL2qXNI/edit#gid=716684045'
-# 스프레스시트 문서 가져오기
-doc = gc.open_by_url(spreadsheet_url)
-# 시트 선택하기
-worksheet = doc.worksheet('시트1')
-
-client = discord.Client()
-
 
 @client.event
 async def on_ready():
