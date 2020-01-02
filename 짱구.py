83764b8fe5de2ef086d5b8c966a60191626ef705
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-03 03:44:11 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -126,8 +126,7 @@ async def on_message(message):
         )
         await client.send_message(message.channel, embed=embed)       
             
- @client.event
- async def on_message(message):
+
             
     if message.content.startswith("!박스"):        
      embed = discord.Embed(title="타이틀!", colour=discord.Color.red())       
