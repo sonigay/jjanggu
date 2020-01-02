638a62305c96a0d63116b587848381493da35494
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-03 03:38:19 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -126,6 +126,10 @@ async def on_message(message):
         )
         await client.send_message(message.channel, embed=embed)       
             
+@client.event
+    if message.content.startswith("!박스"):        
+     embed = discord.Embed(title="타이틀!", colour=discord.Color.red())       
+       await client.send_message(message.channel, embed=embed)     
           
 
 access_token = os.environ["BOT_TOKEN"]
