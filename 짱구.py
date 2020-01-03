b021dcded97290529376589d15ddf25835006d9e
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-04 08:51:49 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -120,8 +120,8 @@ async def on_message(message):
             
      if message.content.startswith('!박스'):
         embed = discord.Embed(title="박스타이틀!", color=0x00ff00)
-        embed.add_field(name="박스1", value= '```내용1```')
-        embed.add_field(name="박스2", value= '```내용2```')
+        embed.add_field(name="박스1", value= '```내용1```', inline=True)
+        embed.add_field(name="박스2", value= '```내용2```', inline=True)
         embed.add_field(name="박스3", value= '```내용3```')
         embed.add_field(name="박스4", value= '```내용4```')
         embed.add_field(name="박스5", value= '```내용5```')
