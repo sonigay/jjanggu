d32ea7f04f56b7968ff039231ada601302daaab1
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-04 07:49:44 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -133,8 +133,8 @@ async def on_message(message):
         
       if message.content.startswith('!박스2'):
         embed = discord.Embed(title="명령어!", color=0x00ff00)
-        embed.add_field(value= '```!박스```')
-        embed.add_field(value= '```!박스2```')
+        embed.add_field(name=" ", value= '```!박스```')
+        embed.add_field(name="", value= '```!박스2```')
         
         await client.send_message(message.channel, embed=embed, tts=False)  
             
