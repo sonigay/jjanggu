9fbc79afe820c68de508bf9e4a2bb4a41c28b318
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-10 23:38:12 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -11,6 +11,18 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+     print("login")
+     print(client.user.name)
+     print(client.user.id)
+     print("----------------")
+     await client.change_presence(game=discord.Game(name='업무지식 안내', type=1))
+     
+     
+     
+     
+     
+@client.event
+async def on_message(message):
      if message.channel.is_private and message.author.id != "538289410018639893":
         await client.send_message(discord.utils.get(client.get_all_memvers(), id="315237238940106754", message.author.name + "(" +message.author.id + ") : " + message.content)
                                   
