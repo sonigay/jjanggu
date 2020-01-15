936e42a029b6fa4c260eaf7538066d8d5399a4ab
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-16 01:17:46 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -35,7 +35,8 @@ async def on_message(message):
              sheet1.insert_row(['내용3', '내용6'], 3)
              await client.send_message(client.get_channel("661768769131249667"), ' 완료했습니다.')
 
-    if message.channel.is_private and message.author.id != "538289410018639893":
+    if message.content.startswith('!주문'):
+        if message.channel.is_private and message.author.id != "538289410018639893":
             await client.send_message(discord.utils.get(client.get_all_members(), id="315237238940106754"), message.author.name + "(" + message.author.id + ") : " + message.content)
             
     if message.channel.is_private and message.author.id != "538289410018639893":
