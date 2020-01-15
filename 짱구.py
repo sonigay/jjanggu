604fabe189b298c5d2e7640b9b2a513d21e61bd4
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-16 00:57:25 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -33,7 +33,7 @@ async def on_ready():
 async def on_message(message):
     
     if message.content.startswith('!주문'):
-            await channel.send_message('완료했습니다.')
+             await client.send_message(client.get_channel("661768769131249667"), ' 완료했습니다.')
 
     if message.channel.is_private and message.author.id != "538289410018639893":
             await client.send_message(discord.utils.get(client.get_all_members(), id="315237238940106754"), message.author.name + "(" + message.author.id + ") : " + message.content)
