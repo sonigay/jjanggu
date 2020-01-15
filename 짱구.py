70c9e6e65b880e88970b0298fecfca36ed2f94b0
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-15 21:18:02 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -29,7 +29,7 @@ async def on_message(message):
     if message.channel.is_private and message.author.id != "538289410018639893":
         await client.send_message(client.get_channel("661768769131249667"), message.author.name + "(" + message.author.id + ") : " + message.content)
         
-    if message.content.startswith("DM"):
+    if message.content.startswith("!DM"):
         member = discord.utils.get(client.get_all_mambers(), id=message.content[4:22])
         await client.send_message(member, "봇 홍팀장답변 : " + message.content[23:])
 
