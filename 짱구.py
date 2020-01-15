e033061f1c9a44ac10bf3ed12b539a38666a07fd
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-16 01:25:11 +0900

    Update 짱구.py

--- 짱구.py
+++ 짱구.py
@@ -32,19 +32,19 @@ async def on_ready():
 async def on_message(message):
     
     if message.content.startswith('!주문'):
-             sheet1.insert_row(['내용3', '내용6'], 3)
-             await client.send_message(client.get_channel("661768769131249667"), ' 완료했습니다.')
-
+        if message.channel.is_private and message.author.id != "538289410018639893":
+            sheet1.insert_row(['내용3', '내용6'], 3)
+            await client.send_message(discord.utils.get(client.get_all_members(), id="315237238940106754"), message.author.name + "(" + message.author.id + ") : " + message.content)
+            await client.send_message(client.get_channel("661768769131249667"), ' 주문이 완료 되었습니다. 부득이 한경우 개인답변 드리도록 하겠습니다.')
+            
     if message.content.startswith('!주문'):
         if message.channel.is_private and message.author.id != "538289410018639893":
-            await client.send_message(discord.utils.get(client.get_all_members(), id="315237238940106754"), message.author.name + "(" + message.author.id + ") : " + message.content)
-            
-    if message.channel.is_private and message.author.id != "538289410018639893":
             await client.send_message(client.get_channel("661768769131249667"), message.author.name + "(" + message.author.id + ") : " + message.content)
-        
+            await client.send_message(client.get_channel("661768769131249667"), ' 주문이 완료 되었습니다. 부득이 한경우 개인답변 드리도록 하겠습니다.')
+            
     if message.content.startswith("!DM"):
             member = discord.utils.get(client.get_all_members(), id=message.content[4:22])
-            await client.send_message(member, "봇 홍팀장답변 : " + message.content[23:])
+            await client.send_message(member, "홍팀장 개인답변 : " + message.content[23:])
 
                         
 access_token = os.environ["BOT_TOKEN"]
